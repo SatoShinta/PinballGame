fe9910c6ad0b180f09116dd9a6d51b2eb57c5d21
SatoShinta <kanten3005@gmail.com>
2024-07-25 19:54:42 +0900

    アニメーション　タイムラインで編集

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,24 +6,30 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] GameObject Ball;
     [SerializeField] Text nowBallYText;
     [SerializeField] Text bestBallYText;
+    [SerializeField] Text bestScoreText;
     [SerializeField] GameManager manager;
 
     float ballPosY;
     float bestPosy;
     public static float bestBallY;
+    public static float score;
 
 
     private void Start()
     {
+        bestBallY = 0;
+        score = 10;
         ballPosY = 0;
-       // bestPosy = 0;
+        // bestPosy = 0;
+
+        bestScoreText.text = score.ToString();
         bestBallYText.text = bestBallY.ToString();
         manager = FindObjectOfType<GameManager>();
     }
 
     private void Update()
     {
-        if(Ball != null )
+        if (Ball != null)
         {
             nowBallYText.text = ballPosY.ToString();
             ballPosY = Ball.transform.position.y;
@@ -37,12 +43,12 @@ public class ScoreManager : MonoBehaviour
                 ballPosY = 0;
             }
         }
-      
+
     }
 
     void BestBallPosUpdate()
     {
-        if(Ball != null)
+        if (Ball != null)
         {
             bestPosy = ballPosY;
 
@@ -52,6 +58,12 @@ public class ScoreManager : MonoBehaviour
                 bestBallYText.text = bestBallY.ToString();
             }
         }
-      
+
+    }
+    public void ResultScore()
+    {
+        bestBallY += score;
+        bestBallYText.text = bestBallY.ToString() ;
+        Debug.Log(bestBallY);
     }
 }
