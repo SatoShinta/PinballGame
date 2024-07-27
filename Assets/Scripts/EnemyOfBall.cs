0bbdca074ede0b6728fb9fd2bb58be82ab8edb3f
SatoShinta <kanten3005@gmail.com>
2024-07-27 13:23:34 +0900

    comment追加

--- Assets/Scripts/EnemyOfBall.cs
+++ Assets/Scripts/EnemyOfBall.cs
@@ -6,21 +6,28 @@ public class EnemyOfBall : MonoBehaviour
 {
     [SerializeField, Header("敵ボール")] GameObject madeOfEnemy;
 
+    //ボールが当たった数
     int hitCounter;
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        //ballに当たったら
         if (collision.gameObject.CompareTag("Ball"))
         {
+            //敵からボールを生成し、その後敵を破壊する
             Instantiate(madeOfEnemy,transform.position, Quaternion.identity);
             Invoke("EnemyBall", 0.2f);
         }
 
+        //敵から生成したボールに当たったら
         if (collision.gameObject.CompareTag("EnemyBall"))
         {
+            //hitCounterを増やし
             hitCounter++;
+            //ヒットカウンターが３になったら
             if(hitCounter == 3)
             {
+                //このオブジェクトを破壊する
                 EnemyBall();
             }
         }
