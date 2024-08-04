ac61dca2ac8724baf9f4d6421508e307a2e24f23
SatoShinta <kanten3005@gmail.com>
2024-08-04 21:57:41 +0900

    3層の敵を追加

--- Assets/Scripts/BallCtr.cs
+++ Assets/Scripts/BallCtr.cs
@@ -6,6 +6,7 @@ public class BallCtr : MonoBehaviour
     [SerializeField] GameManager gameManager;
     [SerializeField] GameObject[] taihou;
     [SerializeField] ScoreManager scoreManager;
+    [SerializeField] EnemyManager EnemyManager;
 
     Rigidbody2D rigidbody2;
     Collider2D taihoucol;
@@ -16,20 +17,40 @@ public class BallCtr : MonoBehaviour
         rigidbody2 = GetComponent<Rigidbody2D>();
     }
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("taihou"))
-        {
-            rigidbody2.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
-        }
-        else if (collision.gameObject.CompareTag("Enemy"))
-        {
-            scoreManager.ScoreUpdate(1);
-        }
-        else if (collision.gameObject.CompareTag("Enemy2"))
-        {
-            scoreManager.ScoreUpdate(2);
-        }
 
+        //ëÂñCÇ∆ìGÇ…ìñÇΩÇ¡ÇΩéûÇÃèàóù
+        switch (collision.gameObject.tag)
+        {
+            case "taihou":
+                rigidbody2.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+                break;
+            case "Enemy":
+                scoreManager.ScoreUpdate(1);
+                break;
+            case "Enemy2":
+                scoreManager.ScoreUpdate(2);
+                break;
+            case "Enemy3":
+                scoreManager.ScoreUpdate(3);
+                break;
+            default:
+                break;
+        }
+        if (collision.gameObject.Equals(EnemyManager.enemy3action))
+        {
+           EnemyManager.enemy_3_spawn = true;
+        }
     }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.Equals(EnemyManager.enemy3action))
+        {
+            EnemyManager.enemy_3_spawn = true;
+        }
+    }
+
 }
--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -5,22 +5,42 @@ public class EnemyManager : MonoBehaviour
 {
     [SerializeField, Header("敵たち1")] List<GameObject> enemys1;
     [SerializeField, Header("敵たち2")] List<GameObject> enemys2;
+    [SerializeField, Header("敵たち3")] List<GameObject> enemys3;
     [SerializeField, Header("1層出現位置")] List<GameObject> spownpos1;
     [SerializeField, Header("2層出現位置")] List<GameObject> spownpos2;
+    [SerializeField, Header("3層出現位置")] List<GameObject> spownpos3;
+    public bool enemy_3_spawn;
+    public GameObject enemy3action;
 
     public void Start()
     {
+        enemy_3_spawn = false;
+
         // スポーン位置を自動で設定
         SpawnPositions("spawn1", spownpos1);
         SpawnPositions("spawn2", spownpos2);
+        SpawnPositions("spawn3", spownpos3);
 
         // 敵を生成
         EnemyInstantiate1();
         EnemyInstantiate2();
     }
 
+    // enemy_3_spawnフラグがONになったら3種類目の敵を出現させる
+    public void Update()
+    {
+        if(enemy3action != null)
+        {
+            if (enemy_3_spawn)
+            {
+                EnemyInstantiate3();
+            }
+        }
+        
+    }
 
-    // 汎用メソッド: 敵を生成する
+
+    //  敵を生成する
     private void InstantiateEnemies(List<GameObject> enemies, List<GameObject> spawnPositions)
     {
         // 出現位置リストが空でない場合
@@ -44,6 +64,12 @@ public class EnemyManager : MonoBehaviour
     public void EnemyInstantiate2()
     {
         InstantiateEnemies(enemys2, spownpos2);
+    }
+
+    //　3層の敵を生成する
+    public void EnemyInstantiate3()
+    {
+        InstantiateEnemies(enemys3, spownpos3);
     }
 
     // 指定した名前で出現位置を初期化する
