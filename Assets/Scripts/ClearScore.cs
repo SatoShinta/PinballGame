e8ed910baaa33c443f083a200f66fec20f06d123
SatoShinta <kanten3005@gmail.com>
2024-08-07 18:27:56 +0900

    ゲームの流れが完成した！

--- Assets/Scripts/ClearScore.cs
+++ Assets/Scripts/ClearScore.cs
@@ -25,32 +25,40 @@ public class ClearScore : MonoBehaviour
    
     public void CleaScore1()
     {
-        saisyutokuten.text = ("最高地点 :" + ScoreManager.bestBallY.ToString());
-        tuikatokuten.text = ("追加得点 :" + ScoreManager.extraScore.ToString());
-        kuriataimutokuten.text = ("クリアタイムボーナス :" + Mathf.CeilToInt(GameManager.gameOverTimer).ToString());
-        tekigekihatokuten.text = ("敵撃破ボーナス :" + BallCtr.destroyCounter2.ToString());
-        arigatoutokuten.text = ("クリアまで遊んでくれてありがとう！");
+        saisyutokuten.text = ("最終得点 : " + ScoreManager.bestBallY.ToString());
+        tuikatokuten.text = ("追加得点 : " + ScoreManager.extraScore.ToString());
+        kuriataimutokuten.text = ("クリアタイムボーナス : " + Mathf.CeilToInt(GameManager.gameOverTimer).ToString());
+        tekigekihatokuten.text = ("敵撃破ボーナス : " + BallCtr.destroyCounter2.ToString());
+        arigatoutokuten.text = ("最後まで遊んでくれてありがとう！");
         arigatousukoa.text = (bonasuscore.ToString());
     }
 
     public void ClearScoreAdd1()
     {
         ScoreManager.bestBallY += ScoreManager.extraScore;
+        saisyutokuten.text = ("最終得点 :" + ScoreManager.bestBallY.ToString());
+
     }
 
     public void ClearScoreAdd2()
     {
         ScoreManager.bestBallY += (int)GameManager.gameOverTimer;
+        saisyutokuten.text = ("最終得点 :" + ScoreManager.bestBallY.ToString());
+
     }
 
     public void ClearScoreAdd3()
     {
         ScoreManager.bestBallY += BallCtr.destroyCounter2;
+        saisyutokuten.text = ("最終得点 :" + ScoreManager.bestBallY.ToString());
+
     }
 
     public void ClearScoreAdd4()
     {
         ScoreManager.bestBallY += bonasuscore;
+        saisyutokuten.text = ("最終得点 :" + ScoreManager.bestBallY.ToString());
+
     }
 
     public void saisyuutokuten()
